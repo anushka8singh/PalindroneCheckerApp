0ecd8d00471c24e3361677c01f7f0a513e028ee9
anushka8singh <anushka0506singh@gmail.com>
2026-02-23 11:11:18 +0530

    anushka added UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
 
-        public static boolean isPalindrome(String s){
-            int n= s.length();
-            for(int i=0;i<n/2;i++){
-                if(s.charAt(i)!=s.charAt(n-i-1)){
-                    return false;
-                }
-        }
-            return true;
 
-    }
     public static void main(String[] args){
+
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter a string :");
             String s = sc.nextLine();
-            if(isPalindrome(s)){
-                System.out.println("Palindrome");
+            String rev ="";
+             for(int i=s.length()-1;i>=0;i--){
+                 rev+=s.charAt(i);
+             }
+            if(s.equals(rev)){
+                System.out.print("Palindrome");
             }else{
                 System.out.println("Not a Palindrome");
             }
