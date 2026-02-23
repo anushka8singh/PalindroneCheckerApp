51c66ca14758328cf7019eb4044cb8cf265b31c6
anushka8singh <anushka0506singh@gmail.com>
2026-02-23 10:31:58 +0530

    anushka added UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,21 @@
 public class PalindromeCheckerApp {
-    public static void main (String[] args){
-        System.out.println("welcome to Palindrome Checker App management system");
-        System.out.println("Modify this logic to usecase 2");
+
+        public static boolean isPalindrome(String s){
+            int n= s.length();
+            for(int i=0;i<n/2;i++){
+                if(s.charAt(i)!=s.charAt(n-i-1)){
+                    return false;
+                }
+        }
+            return true;
+
+    }
+    public static void main(String[] args){
+            String s = "madam";
+            if(isPalindrome(s)){
+                System.out.println("Palindrome");
+            }else{
+                System.out.println("Not a Palindrome");
+            }
     }
 }
