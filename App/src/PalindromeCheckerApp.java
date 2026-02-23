f5092e25788a5f0375926c26a08054e69c9c32df
anushka8singh <anushka0506singh@gmail.com>
2026-02-23 09:44:23 +0530

    anushka added UC2-HardCodePalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
 public class PalindromeCheckerApp {
     public static void main (String[] args){
         System.out.println("welcome to Palindrome Checker App management system");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
