04464cc2bf9a22100e19af144d00faf3e30811bd
anushka8singh <anushka0506singh@gmail.com>
2026-03-01 01:23:55 +0530

    anushka added UC3-StringReverse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,7 +4,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args){
 
-            Scanner sc = new Scanner(System.in);
+           /*  Scanner sc = new Scanner(System.in);
             System.out.print("Enter a string :");
             String s = sc.nextLine();
             String rev ="";
@@ -15,7 +15,25 @@ public class PalindromeCheckerApp {
                 System.out.print("Palindrome");
             }else{
                 System.out.println("Not a Palindrome");
-            }
+            }*/
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        
+        if (original.equals(reversed)) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+
             sc.close();
     }
 }
